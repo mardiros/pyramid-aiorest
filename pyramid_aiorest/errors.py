c2e05a54cd3d9d41b96ca2fbed20a8de3193778c
Guillaume Gauvrit <guillaume@gauvr.it>
2015-02-13 22:04:56 +0100

    Render pyramid http exception in json

--- pyramid_aiorest/errors.py
+++ pyramid_aiorest/errors.py
@@ -3,11 +3,12 @@ import simplejson as json
 from pyramid.response import Response
 from pyramid.view import view_config
 from pyramid_yards import ValidationFailure
+from pyramid import httpexceptions
 
 from .i18n import gettext as _
 
 @view_config(context=ValidationFailure)
-def http_exception(exc, request):
+def validation_failure(exc, request):
     # If the view has two formal arguments, the first is the context.
     # The context is always available as ``request.context`` too.
     error = {
@@ -19,3 +20,41 @@ def http_exception(exc, request):
     response.status_int = 422
     return response
 
+
+@view_config(context=httpexceptions.HTTPBadRequest)
+@view_config(context=httpexceptions.HTTPUnauthorized)
+@view_config(context=httpexceptions.HTTPPaymentRequired)
+@view_config(context=httpexceptions.HTTPForbidden)
+@view_config(context=httpexceptions.HTTPNotFound)
+@view_config(context=httpexceptions.HTTPMethodNotAllowed)
+@view_config(context=httpexceptions.HTTPNotAcceptable)
+@view_config(context=httpexceptions.HTTPProxyAuthenticationRequired)
+@view_config(context=httpexceptions.HTTPRequestTimeout)
+@view_config(context=httpexceptions.HTTPConflict)
+@view_config(context=httpexceptions.HTTPGone)
+@view_config(context=httpexceptions.HTTPLengthRequired)
+@view_config(context=httpexceptions.HTTPPreconditionFailed)
+@view_config(context=httpexceptions.HTTPRequestEntityTooLarge)
+@view_config(context=httpexceptions.HTTPRequestURITooLong)
+@view_config(context=httpexceptions.HTTPUnsupportedMediaType)
+@view_config(context=httpexceptions.HTTPRequestRangeNotSatisfiable)
+@view_config(context=httpexceptions.HTTPExpectationFailed)
+@view_config(context=httpexceptions.HTTPUnprocessableEntity)
+@view_config(context=httpexceptions.HTTPLocked)
+@view_config(context=httpexceptions.HTTPFailedDependency)
+@view_config(context=httpexceptions.HTTPInternalServerError)
+@view_config(context=httpexceptions.HTTPNotImplemented)
+@view_config(context=httpexceptions.HTTPBadGateway)
+@view_config(context=httpexceptions.HTTPServiceUnavailable)
+@view_config(context=httpexceptions.HTTPGatewayTimeout)
+@view_config(context=httpexceptions.HTTPVersionNotSupported)
+@view_config(context=httpexceptions.HTTPInsufficientStorage)
+def http_exception(exc, request):
+    error = {
+        'message': exc.title,
+        'explanation': exc.explanation,
+    }
+    response = Response(json.dumps(error))
+    response.content_type = 'application/json'
+    response.status_int = exc.status_int
+    return response
